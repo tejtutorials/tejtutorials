c99a988f1a5d4eb23804b186bffbac3183db471a
tejtutorials <tejtutorialsnerul@gmail.com>
2024-10-27 10:18:12 +0530

    commit

--- static/assets/js/color-modes.js
+++ static/assets/js/color-modes.js
@@ -20,8 +20,8 @@
   }
 
   const setTheme = theme => {
-    if (theme === 'auto') {
-      document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
+    if (theme === 'dark') {
+      document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: light)').matches ? 'dark' : 'light'))
     } else {
       document.documentElement.setAttribute('data-bs-theme', theme)
     }
